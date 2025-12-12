a83b76c933dafb6e34dc73537b98b04c5366d97e
XyyCi1 <bavikx@gmail.com>
2025-12-12 10:19:06 +0800

    sdf

--- NavarreteTTEProblem1/Form1.Designer.vb
+++ NavarreteTTEProblem1/Form1.Designer.vb
@@ -22,10 +22,157 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        TextBoxName = New TextBox()
+        TextBoxOrigin = New TextBox()
+        TextBoxYRDisc = New TextBox()
+        ComboBoxCondition = New ComboBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        ButtonSave = New Button()
+        ButtonUpdate = New Button()
+        ButtonDelete = New Button()
+        ButtonLoad = New Button()
+        DataGridView1 = New DataGridView()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' TextBoxName
+        ' 
+        TextBoxName.Location = New Point(42, 57)
+        TextBoxName.Name = "TextBoxName"
+        TextBoxName.Size = New Size(100, 23)
+        TextBoxName.TabIndex = 0
+        ' 
+        ' TextBoxOrigin
+        ' 
+        TextBoxOrigin.Location = New Point(160, 57)
+        TextBoxOrigin.Name = "TextBoxOrigin"
+        TextBoxOrigin.Size = New Size(100, 23)
+        TextBoxOrigin.TabIndex = 0
+        ' 
+        ' TextBoxYRDisc
+        ' 
+        TextBoxYRDisc.Location = New Point(278, 57)
+        TextBoxYRDisc.Name = "TextBoxYRDisc"
+        TextBoxYRDisc.Size = New Size(100, 23)
+        TextBoxYRDisc.TabIndex = 0
+        ' 
+        ' ComboBoxCondition
+        ' 
+        ComboBoxCondition.FormattingEnabled = True
+        ComboBoxCondition.Items.AddRange(New Object() {"Excellent", "Good", "Fair", "Poor"})
+        ComboBoxCondition.Location = New Point(413, 57)
+        ComboBoxCondition.Name = "ComboBoxCondition"
+        ComboBoxCondition.Size = New Size(121, 23)
+        ComboBoxCondition.TabIndex = 1
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(42, 39)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(81, 15)
+        Label1.TabIndex = 2
+        Label1.Text = "Artifact Name"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(160, 39)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(86, 15)
+        Label2.TabIndex = 2
+        Label2.Text = "Origin Country"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(278, 39)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(93, 15)
+        Label3.TabIndex = 2
+        Label3.Text = "Year Discovered:"
+        ' 
+        ' ButtonSave
+        ' 
+        ButtonSave.Location = New Point(67, 115)
+        ButtonSave.Name = "ButtonSave"
+        ButtonSave.Size = New Size(75, 23)
+        ButtonSave.TabIndex = 3
+        ButtonSave.Text = "Save"
+        ButtonSave.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonUpdate
+        ' 
+        ButtonUpdate.Location = New Point(171, 115)
+        ButtonUpdate.Name = "ButtonUpdate"
+        ButtonUpdate.Size = New Size(75, 23)
+        ButtonUpdate.TabIndex = 3
+        ButtonUpdate.Text = "Update"
+        ButtonUpdate.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonDelete
+        ' 
+        ButtonDelete.Location = New Point(278, 115)
+        ButtonDelete.Name = "ButtonDelete"
+        ButtonDelete.Size = New Size(75, 23)
+        ButtonDelete.TabIndex = 3
+        ButtonDelete.Text = "Delete"
+        ButtonDelete.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonLoad
+        ' 
+        ButtonLoad.Location = New Point(377, 115)
+        ButtonLoad.Name = "ButtonLoad"
+        ButtonLoad.Size = New Size(75, 23)
+        ButtonLoad.TabIndex = 3
+        ButtonLoad.Text = "Load"
+        ButtonLoad.UseVisualStyleBackColor = True
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Location = New Point(56, 195)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.Size = New Size(538, 181)
+        DataGridView1.TabIndex = 4
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(DataGridView1)
+        Controls.Add(ButtonLoad)
+        Controls.Add(ButtonDelete)
+        Controls.Add(ButtonUpdate)
+        Controls.Add(ButtonSave)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(ComboBoxCondition)
+        Controls.Add(TextBoxYRDisc)
+        Controls.Add(TextBoxOrigin)
+        Controls.Add(TextBoxName)
+        Name = "Form1"
+        Text = "Form1"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents TextBoxName As TextBox
+    Friend WithEvents TextBoxOrigin As TextBox
+    Friend WithEvents TextBoxYRDisc As TextBox
+    Friend WithEvents ComboBoxCondition As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents ButtonSave As Button
+    Friend WithEvents ButtonUpdate As Button
+    Friend WithEvents ButtonDelete As Button
+    Friend WithEvents ButtonLoad As Button
+    Friend WithEvents DataGridView1 As DataGridView
 
 End Class
--- NavarreteTTEProblem1/Form1.vb
+++ NavarreteTTEProblem1/Form1.vb
@@ -1,3 +1,146 @@
-﻿Public Class Form1
+﻿Imports MySql.Data.MySqlClient
 
+Public Class Form1
+    Dim conn As MySqlConnection
+    Dim COMMAND As MySqlCommand
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub ButtonSave_Click(sender As Object, e As EventArgs) Handles ButtonSave.Click
+
+
+        Dim query As String = "INSERT INTO `museum_db`.`artifacts_tbl` (`artifact_name`, `country`, `year_found`, `condition`) VALUES (@artifact_name, @country, @year_found, @condition);"
+
+        Try
+            Using conn As New MySqlConnection("Server = localhost;userid=root;password=root;database=museum_db;")
+                conn.Open()
+                Using cmd As New MySqlCommand(query, conn)
+                    cmd.Parameters.AddWithValue("@artifact_name", TextBoxName.Text)
+                    cmd.Parameters.AddWithValue("@country", TextBoxOrigin.Text)
+                    cmd.Parameters.AddWithValue("@year_found", CInt(TextBoxYRDisc.Text))
+                    cmd.Parameters.AddWithValue("@condition", ComboBoxCondition.Text)
+
+                    cmd.ExecuteNonQuery()
+
+                    MessageBox.Show("Record Inserted Successfully")
+
+                End Using
+            End Using
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub ButtonLoad_Click(sender As Object, e As EventArgs) Handles ButtonLoad.Click
+        Dim query As String = "SELECT * FROM museum_db.artifacts_tbl;"
+
+        Try
+
+            Using conn As New MySqlConnection("server=localhost;userid=root;password=root;database=museum_db;")
+                Dim adapter As New MySqlDataAdapter(query, conn) ' get from
+                Dim table As New DataTable() ' table object
+                adapter.Fill(table) ' from adapter to table object
+                DataGridView1.DataSource = table ' display to DataGridView
+            End Using
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub ButtonDelete_Click(sender As Object, e As EventArgs) Handles ButtonDelete.Click
+        If DataGridView1.SelectedRows.Count > 0 Then
+            Dim selectedRow As DataGridViewRow = DataGridView1.SelectedRows(0)
+            Dim id As Integer = CInt(selectedRow.Cells("id").Value)
+
+            Dim query As String = "DELETE FROM artifacts_tbl WHERE id = @id"
+            Try
+                Using conn As New MySqlConnection("server=localhost;userid=root;password=root;database=museum_db;")
+                    conn.Open()
+                    Using cmd As New MySqlCommand(query, conn)
+                        cmd.Parameters.AddWithValue("@id", id)
+                        Dim rowsAffected As Integer = cmd.ExecuteNonQuery()
+                        If rowsAffected > 0 Then
+                            MessageBox.Show("Record Deleted Successfully")
+                        Else
+                            MessageBox.Show("Delete Failed: No matching record found")
+                        End If
+                    End Using
+                End Using
+            Catch ex As Exception
+                MessageBox.Show("Error: " & ex.Message)
+            End Try
+        Else
+            MessageBox.Show("Please select a row to delete")
+        End If
+    End Sub
+
+    Private Sub ButtonUpdate_Click(sender As Object, e As EventArgs) Handles ButtonUpdate.Click
+        DataGridView1.EndEdit()
+        BindingContext(DataGridView1.DataSource).EndCurrentEdit()
+
+        Dim rowIndex As Integer = DataGridView1.CurrentCell.RowIndex
+        Dim selectedRow As DataGridViewRow = DataGridView1.Rows(rowIndex)
+        Dim idValue = selectedRow.Cells("id").Value
+        Dim nameValue = selectedRow.Cells("artifact_name").Value
+        Dim countryValue = selectedRow.Cells("country").Value
+        Dim YearFoundValue = selectedRow.Cells("year_found").Value
+        Dim ConditionValue = selectedRow.Cells("condition").Value
+
+
+
+        If idValue IsNot Nothing AndAlso idValue.ToString().Trim() <> "" AndAlso Integer.TryParse(idValue.ToString(), Nothing) Then
+            Dim query As String = "UPDATE museum_db.artifacts_tbl 
+                        SET artifact_name = @artifact_name,
+                            country = @country,
+                            year_found = @year_found,
+                            `condition` = @condition
+                        WHERE id = @id;"
+
+
+
+            Try
+                Using conn As New MySqlConnection("server=localhost;userid=root;password=root;database=museum_db;")
+                    conn.Open()
+                    Using cmd As New MySqlCommand(query, conn)
+                        cmd.Parameters.AddWithValue("@artifact_name", nameValue.ToString())
+                        cmd.Parameters.AddWithValue("@country", countryValue)
+                        cmd.Parameters.AddWithValue("@year_found", CInt(YearFoundValue))
+                        cmd.Parameters.AddWithValue("@condition", ConditionValue)
+                        cmd.Parameters.AddWithValue("@id", Convert.ToInt32(idValue))
+
+                        Dim rowsAffected As Integer = cmd.ExecuteNonQuery()
+                        If rowsAffected > 0 Then
+                            MessageBox.Show("Record Updated Successfully")
+                            ButtonLoad_Click(Nothing, Nothing)
+                        Else
+                            MessageBox.Show("Update Failed: No matching record found")
+                        End If
+                    End Using
+                End Using
+
+            Catch ex As Exception
+                MessageBox.Show("Error: " & ex.Message)
+            End Try
+        Else
+            MessageBox.Show("ID is invalid. Please select a valid row.")
+        End If
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellClick
+        If e.RowIndex >= 0 Then
+            Dim row As DataGridViewRow = DataGridView1.Rows(e.RowIndex)
+            TextBoxName.Text = row.Cells("artifact_name").Value.ToString()
+            TextBoxOrigin.Text = row.Cells("country").Value.ToString()
+            TextBoxYRDisc.Text = row.Cells("year_found").Value.ToString()
+
+            ComboBoxCondition.Text = row.Cells("condition").Value.ToString()
+
+
+
+
+        End If
+    End Sub
 End Class
